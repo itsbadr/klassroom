509d813e122bffad92c4649cb0fbd43865b6d9f3
Badr <badrkmc@gmail.com>
2021-02-17 23:47:30 +0500

    setup a route to get courses

--- server/main.py
+++ server/main.py
@@ -2,13 +2,31 @@ from googleapiclient.discovery import build
 
 from auth import credentials
 
+from flask import Flask
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
 service = build("classroom", "v1", credentials=credentials)
 
-results = service.courses().list(pageSize=8).execute()
-courses = results.get("courses", [])
 
-if not courses:
-    pass
-else:
-    for course in courses:
-        print(course.courseMaterialSets)
+@app.route('/courses', methods=["GET"])
+def get_courses():
+
+    results = service.courses().list(pageSize=8).execute()
+    courses = results.get("courses", [])
+
+    added_courses = {}
+
+    if not courses:
+        return {
+            "message": "No courses found"
+        }
+    else:
+        for course in courses:
+            added_courses[course["name"]] = course["id"]
+
+    return added_courses
+
+
+app.run()
