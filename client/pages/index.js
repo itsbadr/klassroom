18507f93f17f5450c0ec193ed60621b6ee380bba
Badr <badrkmc@gmail.com>
2021-02-18 23:41:56 +0500

    some basic styling

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,9 +2,9 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <main>
-        <h1>Welcome!</h1>
+        <h1 style={{ margin: 5, fontSize: 50 }}>Welcome!</h1>
       </main>
     </div>
   )
