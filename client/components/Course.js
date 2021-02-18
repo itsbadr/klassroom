d0034b8bb3a870916b234fe54fb4423b525c303b
Badr <badrkmc@gmail.com>
2021-02-19 00:41:00 +0500

    render courses

---
+++ client/components/Course.js
@@ -0,0 +1,5 @@
+export default function Course({ course }) {
+    return (
+        <div>{course.name}</div>
+    )
+}
---
+++ client/components/Courses.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+import Course from "./Course";
+
+export default function Courses({ }) {
+
+    const [courses, setCourses] = useState([])
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:5000/courses")
+            .then(res => res.json())
+            .then(({ courses }) => setCourses(courses))
+            .catch(error => {
+                console.log(error);
+            })
+    }, []);
+
+    return (
+        <div>
+            {
+                courses.length > 0 ?
+                    courses.map((course) => {
+                        return <Course key={course.id} course={course} />
+                    })
+                    :
+                    <div>No courses</div>
+            }
+        </div>
+    )
+}
