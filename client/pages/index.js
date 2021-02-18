e7505a463cd180d71ccff29fe41222212817f6f1
Badr <badrkmc@gmail.com>
2021-02-19 00:42:36 +0500

    make a courses component for rendering each course

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,13 @@
 import styles from "../styles/Home.module.css";
 
+import Courses from "../components/Courses";
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <main>
         <h1 style={{ margin: 5, fontSize: 50 }}>Welcome!</h1>
+        <Courses />
       </main>
     </div>
   )
