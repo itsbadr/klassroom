0f7dd86e67aeaa66ae32cbad450cce5e8d7d3a39
Badr <badrkmc@gmail.com>
2021-02-19 00:10:18 +0500

    flask cors

--- server/main.py
+++ server/main.py
@@ -3,9 +3,11 @@ from googleapiclient.discovery import build
 from auth import credentials
 
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+CORS(app)
 
 service = build("classroom", "v1", credentials=credentials)
 
