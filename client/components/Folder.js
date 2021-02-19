45f25815012d799ec81b3b7aaac9b37c82a05d93
Badr <badrkmc@gmail.com>
2021-02-19 16:21:13 +0500

    folders now use folder svg instead of a div

--- client/components/Folder.js
+++ client/components/Folder.js
@@ -3,8 +3,7 @@ import styles from "../styles/Folder.module.css";
 export default function Folder({ folder }) {
     return (
         <div style={{ maxWidth: "70px" }}>
-            <div className={styles.folder}>
-            </div>
+            <img style={{ filter: "invert(77%) sepia(48%) saturate(6389%) hue-rotate(147deg) brightness(104%) contrast(73%)", margin: "15px" }} src="../folder.svg" alt="folder" />
             <h6 style={{ margin: 0, textAlign: "center", wordWrap: "break-word" }}>{folder.name}</h6>
         </div>
 
