e982cef289cec1350da404ca2d0aca4a8a3717d6
Badr <badrkmc@gmail.com>
2021-02-19 11:43:53 +0500

    make folder prototype

--- client/components/Klassroom.js
+++ client/components/Klassroom.js
@@ -1,7 +1,39 @@
+import { useState } from "react"
+
 export default function Klassroom() {
+
+    const [folders, makeFolder] = useState([{
+        folderName: "folder",
+        folderId: 123
+    }])
+
+    function makeNewFolder() {
+
+        const folder = {
+            folderName: "newFolder",
+            folderId: 123
+        }
+
+        makeFolder(oldFolder => [...oldFolder, folder])
+
+    }
+
     return (
         <div>
             <h2>Klassroom</h2>
+            <button onClick={makeNewFolder}>
+                Create a new folder
+            </button>
+            <div>
+                {
+                    folders.map((folder) => {
+                        return (
+                            <div key={folder.id}>{folder.folderName}</div>
+                        )
+                    })
+                }
+            </div>
+
         </div>
     )
 }
