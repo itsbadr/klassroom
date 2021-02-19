676bba32264e9d5e525f9c9496969f78fed7f241
Badr <badrkmc@gmail.com>
2021-02-19 19:57:31 +0500

    proper font weights

--- client/components/Folder.js
+++ client/components/Folder.js
@@ -4,7 +4,7 @@ export default function Folder({ folder }) {
     return (
         <div className={styles.folderContainer}>
             <img className={styles.folderImage} src="../folder.svg" alt="folder" />
-            <h6 style={{ margin: 0, textAlign: "center", wordWrap: "break-word" }}>{folder.name}</h6>
+            <h6 style={{ margin: 0, fontWeight: 600, textAlign: "center", wordWrap: "break-word" }}>{folder.name}</h6>
         </div>
 
     )
--- client/components/Nav.js
+++ client/components/Nav.js
@@ -4,8 +4,8 @@ export default function Nav() {
     return (
         <nav className={styles.nav}>
             <div>
-                <h2 style={{ margin: "0px 10px" }}>KLASSROOM</h2>
-                <h5 style={{ margin: "0px 10px" }}>Google Classroom, but better <code>....hopefully</code></h5>
+                <h2 style={{ margin: "0px 7px", fontSize: 50, fontWeight: 800 }}>Klassroom</h2>
+                <h5 style={{ margin: "0px 10px", fontWeight: 600 }}>Google Classroom, but better <code>....hopefully</code></h5>
             </div>
         </nav>
     )
