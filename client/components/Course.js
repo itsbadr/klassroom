f7c7cdf2585f78fdf72f94d5102ca44f2b028b00
Badr <badrkmc@gmail.com>
2021-02-19 16:34:22 +0500

    courses now redirect to alternative links of courses

--- client/components/Course.js
+++ client/components/Course.js
@@ -5,19 +5,24 @@ import styles from "../styles/Course.module.css";
 export default function Course({ course }) {
 
     const [courseHovered, setCourseHovered] = useState(false);
-
+    console.log(course);
     return (
-        <div className={styles.course}
-            onMouseEnter={() => setCourseHovered(true)}
-            onMouseLeave={() => setCourseHovered(false)}>
-            <div>
-                <h4
-                    className={styles.courseText}>{course.name}
-                </h4>
-                <h6
-                    style={{ display: courseHovered ? "block" : "none" }}
-                    className={styles.courseText}>{course.id}</h6>
-            </div>
-        </div>
+        <>
+            <a style={{ textDecoration: "none", color: "inherit" }} href={course.alternateLink} target="_blank">
+                <div className={styles.course}
+                    onMouseEnter={() => setCourseHovered(true)}
+                    onMouseLeave={() => setCourseHovered(false)}>
+
+                    <div>
+                        <h4
+                            className={styles.courseText}>{course.name}
+                        </h4>
+                        <h6
+                            style={{ display: courseHovered ? "block" : "none" }}
+                            className={styles.courseText}>{course.id}</h6>
+                    </div>
+                </div>
+            </a>
+        </>
     )
 }
