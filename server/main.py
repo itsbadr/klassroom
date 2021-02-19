892a4dfa8e49bca357895d2257f20ed52d28542d
Badr <badrkmc@gmail.com>
2021-02-19 13:04:10 +0500

    mongoDB DB connection for backend

--- server/main.py
+++ server/main.py
@@ -1,13 +1,21 @@
-from googleapiclient.discovery import build
-
+import pymongo
 from auth import credentials
-
+from config import Config
+from googleapiclient.discovery import build
 from flask import Flask
 from flask_cors import CORS
+
+# Flask
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 CORS(app)
+
+# MongoDB
+
+client = pymongo.MongoClient(Config.MONGO_URI)
+klassroom_database = client["klassroom"]
+folder_collection = klassroom_database["folders"]
 
 service = build("classroom", "v1", credentials=credentials)
 
