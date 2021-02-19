e76082013d0dba8c432e341c82d7d89eb6984de6
Badr <badrkmc@gmail.com>
2021-02-19 22:43:35 +0500

    fix aligning of folders

--- client/components/Folders.js
+++ client/components/Folders.js
@@ -3,7 +3,7 @@ import Folder from "./Folder";
 export default function Folders({ folders }) {
 
     return (
-        <div style={{ display: "flex", flexWrap: "wrap", maxWidth: "300px" }}>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
             {
                 folders.map((folder) => {
                     return (
