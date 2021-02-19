334c0c1ed913ea4bfbf738dbf062610d4dcb84ce
Badr <badrkmc@gmail.com>
2021-02-19 12:13:45 +0500

    make text margin a separate class for css

--- client/components/Courses.js
+++ client/components/Courses.js
@@ -17,19 +17,19 @@ export default function Courses({ }) {
 
     return (
         <div>
-            <h3 style={{ margin: 4 }}>Courses: </h3>
+            <h3 className={styles.coursesText}>Courses: </h3>
             {
                 courses.length > 0 ?
                     courses.map((course) => {
                         return <Course key={course.id} course={course} />
                     })
                     :
-                    <div>No courses</div>
+                    <h3 className={styles.coursesText}>No courses</h3>
             }
             <div>
-                <h3 style={{ margin: 4 }}>Course pagination: {courses.length}</h3>
+                <h3 className={styles.coursesText}>Course pagination: {courses.length}</h3>
                 <div>
-                    <h3 style={{ margin: 4 }}>Change pagination</h3>
+                    <h3 className={styles.coursesText}>Change pagination</h3>
                     <div className={styles.paginationButtons}>
                         <button className={styles.button}>↑</button>
                         <button className={styles.button}>↓</button>
