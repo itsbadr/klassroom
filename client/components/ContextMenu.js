370917febddf32a7b41f94043162ee50cfe748b4
Badr <badrkmc@gmail.com>
2021-02-20 21:08:27 +0500

    styling changes for context menu

--- client/components/ContextMenu.js
+++ client/components/ContextMenu.js
@@ -19,10 +19,14 @@ export default function ContextMenu({ setShowing, showing, top, left, folder, fo
 
     return (
         <div onContextMenu={(event) => {
-            event.preventDefault();
-            setShowing(false);
-        }}
-            onClick={() => setShowing(false)} style={{ display: showing ? "flex" : "none", top, left }} className={styles.contextContainer}>
+                event.preventDefault();
+                setShowing(false);
+            }}
+            onClick={() => setShowing(false)} 
+            style={{ display: showing ? "flex" : "none", top, left }} 
+            className={styles.contextContainer}
+        >
+            <h6 style={{ textAlign: "left", margin: "0px 5px" }}>{folder.name}</h6>
             <div onClick={deleteFolder}>
                 <h5>Delete</h5>
             </div>
