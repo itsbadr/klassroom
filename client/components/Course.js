b718d4df2c367e4deed382659d842bde5da6c903
Badr <badrkmc@gmail.com>
2021-02-21 17:10:05 +0500

    changes to course UI

--- client/components/Course.js
+++ client/components/Course.js
@@ -1,17 +1,12 @@
-import { useState } from "react";
-
 import styles from "../styles/Course.module.css";
 
 export default function Course({ course }) {
-
-    const [courseHovered, setCourseHovered] = useState(false);
 
     return (
         <>
             <a style={{ textDecoration: "none", color: "inherit" }} href={course.alternateLink} target="_blank">
                 <div className={styles.course}
-                    onMouseEnter={() => setCourseHovered(true)}
-                    onMouseLeave={() => setCourseHovered(false)}>
+                >
 
                     <div>
                         {
@@ -27,8 +22,7 @@ export default function Course({ course }) {
                             className={styles.courseText}>{course.name}
                         </h5>
                         <h6
-                            style={{ display: courseHovered ? "block" : "none" }}
-                            className={styles.courseText}>{course.id}</h6>
+                            className={`${styles.courseText} ${styles.courseId}`}>{course.id}</h6>
                     </div>
                 </div>
             </a>
