cf967211cf73979b6846811511e568db8946c6b2
Badr <badrkmc@gmail.com>
2021-02-22 21:26:17 +0500

    add sign in / sign out button in nav

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,15 +1,55 @@
 import styles from "../styles/Nav.module.css";
 
+import { AuthContext } from "../components/AuthContext";
+
+import { useState, useContext } from "react";
+
 export default function Nav() {
+
+    const [ profileName, setProfileName ] = useState(null);
+
+    const [ googleAuth, _ ] = useContext(AuthContext);
+
+    const handleSignIn = () => {
+
+        googleAuth.signIn()
+        .then(() => {
+            setProfileName(googleAuth.currentUser.get().getBasicProfile().getName());
+        });
+
+    }
+
+    const handleSignOut = () => {
+
+        googleAuth.signOut()
+        .then(() => {
+            setProfileName(null);
+        });
+
+    }
+
     return (
         <nav className={styles.nav}>
-            <div>
-                <img className={styles.klassroomImage} src="../klassroom.svg" alt="klassroom" />
+            <div className={styles.navMain}>
+                <div>
+                    <img className={styles.klassroomImage} src="../klassroom.svg" alt="klassroom" />
+                </div>
+                <div>
+                    <h2 className={styles.navTitle}>Klassroom</h2>
+                    <h5 className={styles.navDescription}>{`Welcome${ profileName ? " " + profileName + "!" : "!"}`}</h5>
+                </div>
             </div>
-            <div>
-                <h2 className={styles.navTitle}>Klassroom</h2>
-                <h5 className={styles.navDescription}>Google Classroom, but better <code>....hopefully</code></h5>
-            </div>
+            {
+                profileName
+                ?
+                <div onClick={handleSignOut} className={styles.signButton}>
+                    <h3 style={{ textAlign: "center" }}>SIGN <br />OUT</h3>
+                </div>
+                :
+                <div onClick={handleSignIn} className={styles.signButton}>
+                    <h3 style={{ textAlign: "center" }}>SIGN <br />IN</h3>
+                </div>
+            }
         </nav>
     )
 }
