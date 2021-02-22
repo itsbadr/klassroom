a16be8f46c156f9a9f48579d49f4798ee7ec77c5
Badr <badrkmc@gmail.com>
2021-02-22 16:38:24 +0500

    added logo

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -4,6 +4,9 @@ export default function Nav() {
     return (
         <nav className={styles.nav}>
             <div>
+                <img className={styles.klassroomImage} src="../klassroom.svg" alt="klassroom" />
+            </div>
+            <div>
                 <h2 className={styles.navTitle}>Klassroom</h2>
                 <h5 className={styles.navDescription}>Google Classroom, but better <code>....hopefully</code></h5>
             </div>
