e2caa768bdb8eee2904240aa500a4456ce37e375
Badr <badrkmc@gmail.com>
2021-02-25 17:23:30 +0500

    change scope for extensive access to drive

--- client/utils/utils.js
+++ client/utils/utils.js
@@ -1,2 +1,2 @@
-export const SCOPE         =  "https://www.googleapis.com/auth/drive.file";
+export const SCOPE         =  "https://www.googleapis.com/auth/drive";
 export const discoveryUrl  =  "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest";
