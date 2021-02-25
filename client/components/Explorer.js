d446d08fcfaf18fd23fccfe28898f2160d8d2990
Badr <badrkmc@gmail.com>
2021-02-25 20:04:55 +0500

    google drive integration 😱✨✨

--- client/components/Explorer.js
+++ client/components/Explorer.js
@@ -1,20 +1,52 @@
 import styles from "../styles/Explorer.module.css";
 
-import { useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 
+import { AuthContext } from "../components/AuthContext";
 import { FoldersContext } from "../components/FoldersContext";
+
+import Folder from "./Folder";
 
 export default function Explorer() {
 
+    const [ folderInformation, setFolderInformation ] = useState([]);
+
     const [ folders, updateFolders, 
         currentFolder, setCurrentFolder ] = useContext(FoldersContext);
+    
+    const [ googleAuth, _ ] = useContext(AuthContext);
 
-
+    useEffect(() => {
+        if (googleAuth) {
+            window.gapi.client.request({
+                path: `https://www.googleapis.com/drive/v2/files/${process.env.classroomFolderId}/children`,
+            })
+            .then(({result}) => {
+                result.items.forEach(item => {
+                    window.gapi.client.request({
+                        path: `https://www.googleapis.com/drive/v3/files/${item.id}`,
+                    })
+                    .then(item => {
+                        setFolderInformation(previousFolders => [...previousFolders, item.result])
+                    })
+                    .catch(error => console.log(error))
+                });
+            })
+            .catch(err => console.log(err));
+        }
+    }, [])
 
     return (
         <div className={styles.explorer}>
             <div className={styles.explorerMenu}>
                 <h4>{currentFolder.name}</h4>
+            </div>
+            <div style={{ display: "flex" }}>
+                {
+                folderInformation.map(folder => (
+                    <Folder key={folder.id} folder={folder}/>
+                ))
+                }
             </div>
         </div>
     )
